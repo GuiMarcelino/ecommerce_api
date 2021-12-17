93c86def35f57c8a5ae8039ff87f149cd1763d0c
GuiMarcelino <guilhermemarcelino_91@outlook.com>
2021-12-16 22:02:00 -0300

    devise configure

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,11 @@ Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth/v1/user'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-
   namespace :admin do
     namespace :v1 do
+      get "home" => "home#index"
     end
-end
-
+  end
 
   namespace :storefront do
     namespace :v1 do
